448ade7b5c55e39879d23bff324c0dad0969057a
”Daniel <dannekun@hotmail.com>
2021-05-11 11:49:41 +0200

    first try to commit

---
+++ src/main/java/com/example/springdocker/model/Car.java
@@ -0,0 +1,11 @@
+package com.example.springdocker.model;
+
+/**
+ * Created by Daniel Bojic
+ * Date: 2021-05-11
+ * Time: 11:47
+ * Project: spring-docker-demo
+ * Copyright: MIT
+ */
+public class Car {
+}
