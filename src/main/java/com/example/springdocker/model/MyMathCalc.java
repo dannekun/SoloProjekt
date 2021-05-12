d111fca957f8eb9d3d5a1a1adf42ae3f79bac6e0
”Daniel <dannekun@hotmail.com>
2021-05-12 14:06:50 +0200

    added math method

---
+++ src/main/java/com/example/springdocker/model/MyMathCalc.java
@@ -0,0 +1,27 @@
+package com.example.springdocker.model;
+
+/**
+ * Created by Daniel Bojic
+ * Date: 2021-05-12
+ * Time: 14:04
+ * Project: spring-docker-demo
+ * Copyright: MIT
+ */
+
+
+public class MyMathCalc {
+
+    public int add(int a, int b){
+    return a + b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+
+
+}
